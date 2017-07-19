21a4e152b02f24bfe67b971e820919125ef7be5f
dwqs <461147874@qq.com>
2017-07-20 00:00:44 +0800

    [misc]add dll for webpack

---
+++ template/webpack.dll.config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by pomy on 19/07/2017.
+ */
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        vendor: [
+            'react', 'react-router', 'react-dom', 'prop-types'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: '[name].dll.js',
+        //定义输出：window.${output.library}
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, './dist', '[name]-manifest.json'),
+            // 和 output.library 一样即可
+            name: '[name]_library'
+        })
+    ]
+}

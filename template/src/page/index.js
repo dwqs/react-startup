e9e87c6be0d669ccc6fce8762b7da5fe9daac0e0
dwqs <461147874@qq.com>
2017-07-21 01:17:18 +0800

    [misc]async component

--- template/src/page/index.js
+++ template/src/page/index.js
@@ -4,24 +4,54 @@
 
 import './reset.less';
 
-import React, {Component} from 'react';
+import React, {Component, Children} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { AppContainer } from 'react-hot-loader';
 
-import App from '../general/app/index';
-import Info from '@components/info/index';
-
 const history = createBrowserHistory();
 const env = process.env.NODE_ENV || 'development';
+
+function getAsyncComponent(promise) {
+    return class AsyncComponent extends React.Component {
+
+        constructor(props){
+            super(props);
+            this.state = {
+                Component: null
+            }
+        }
+
+        componentWillMount() {
+            if (!this.state.Component) {
+                promise().then(mod => {
+                    this.setState({
+                        Component: mod.default ? mod.default : mod
+                    })
+                })
+            }
+        }
+        render() {
+            const { Component } = this.state
+            if (Component) {
+                return <Component {...this.props} />
+            }
+            // Failed to resolve async component
+            return null;
+        }
+    }
+}
+
+const App = () => import(/* webpackChunkName: "app1" */ '../general/app/index');
+const Info = () => import(/* webpackChunkName: "info" */ '@components/info/index');
 
 const RootApp = () => {
     return (
         <Router history={history}>
             <Switch>
-                <Route exact path='/' component={App}></Route>
-                <Route path='/info' component={Info}></Route>
+                <Route exact path='/' component={getAsyncComponent(App)}></Route>
+                <Route path='/info' component={getAsyncComponent(Info)}></Route>
             </Switch>
         </Router>
     );

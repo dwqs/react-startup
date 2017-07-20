fb2893c7b83d816279b8bf751a7208f5efc1d7ea
dwqs <461147874@qq.com>
2017-07-20 21:08:59 +0800

    [misc]demo

--- template/src/page/index.js
+++ template/src/page/index.js
@@ -2,20 +2,36 @@
  * Created by pomy on 20/07/2017.
  */
 
-'use strict';
-
 import './reset.less';
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+const Child = ({match}) => {
+    return (
+        <div className="child">{match.params.id}</div>
+    )
+}
+
+const ParamsExample = () => (
+    <Router>
+        <div>
+            <h2>Accounts</h2>
+            <ul>
+                <li><Link to="/netflix">Netflix</Link></li>
+                <li><Link to="/zillow-group">Zillow Group</Link></li>
+                <li><Link to="/yahoo">Yahoo</Link></li>
+                <li><Link to="/modus-create">Modus Create</Link></li>
+            </ul>
+            <Route path="/:id" component={Child}/>
+        </div>
+    </Router>
+)
 window.onload = function () {
     ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={App} />
-        <Route path="/info" component={Info}/>
-        </Router>,
+        <ParamsExample />,
         document.getElementById('app')
     )
 };
+

834d41bf12e48aa44d000fb44984f4daf42fb3bb
dwqs <461147874@qq.com>
2017-07-22 00:07:52 +0800

    [misc]async react component

--- template/src/page/index.js
+++ template/src/page/index.js
@@ -8,40 +8,11 @@ import React, {Component, Children} from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
+import {getAsyncComponent} from 'async-react-component';
 import { AppContainer } from 'react-hot-loader';
 
 const history = createBrowserHistory();
 const env = process.env.NODE_ENV || 'development';
-
-function getAsyncComponent(promise) {
-    return class AsyncComponent extends React.Component {
-
-        constructor(props){
-            super(props);
-            this.state = {
-                Component: null
-            };
-        }
-
-        componentWillMount() {
-            if (!this.state.Component) {
-                promise().then(mod => {
-                    this.setState({
-                        Component: mod.default ? mod.default : mod
-                    });
-                });
-            }
-        }
-        render() {
-            const { Component } = this.state;
-            if (Component) {
-                return <Component {...this.props} />;
-            }
-            // Failed to resolve async component
-            return null;
-        }
-    };
-}
 
 const App = () => import(/* webpackChunkName: "main" */ '../general/app/index');
 const Info = () => import(/* webpackChunkName: "info" */ '@components/info/index');

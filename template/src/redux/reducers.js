52e6e82dfb914e7cfb09089fb8bf0c4755821e0b
dwqs <461147874@qq.com>
2017-07-23 15:27:44 +0800

    [misc]create store

--- template/src/redux/reducers.js
+++ template/src/redux/reducers.js
@@ -1,3 +1,13 @@
 /**
  * Created by pomy on 23/07/2017.
  */
+
+import {combineReducers} from 'redux';
+
+import TimeReducer from './time/reducer';
+import TodoReducer from './todo/reducer';
+
+export  default combineReducers({
+    time: TimeReducer,
+    // todo: TodoReducer
+})
--- template/src/redux/store.js
+++ template/src/redux/store.js
@@ -1,3 +1,9 @@
 /**
  * Created by pomy on 23/07/2017.
  */
+
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+
+export default createStore(reducers);

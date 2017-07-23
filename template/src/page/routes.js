d221da3890ce34bdf93748e879cb59da681ae4c6
dwqs <461147874@qq.com>
2017-07-24 00:45:08 +0800

    [misc]add todo route

--- template/src/page/routes.js
+++ template/src/page/routes.js
@@ -9,11 +9,13 @@ import {getAsyncComponent} from 'async-react-component';
 
 const App = () => import(/* webpackChunkName: "main" */ '../general/app/index');
 const Info = () => import(/* webpackChunkName: "info" */ '@components/info/index');
+const ToDo = () => import(/* webpackChunkName: "todo" */ '../general/todo/index');
 
 const routes =
     <Switch>
         <Route exact path='/' component={getAsyncComponent(App)}></Route>
         <Route path='/info' component={getAsyncComponent(Info)}></Route>
+        <Route path='/todo' component={getAsyncComponent(ToDo)}></Route>
         <Route render={() => <div>404 not found</div>}></Route>
     </Switch>;
 

9f3fcfc4e74f0b6f25e0186761cd0f2372bd50f7
dwqs <461147874@qq.com>
2017-07-24 18:11:08 +0800

    [misc]update

--- template/webpack.dll.config.js
+++ template/webpack.dll.config.js
@@ -7,21 +7,32 @@
 const path = require('path');
 const webpack = require('webpack');
 
+{{#if_eq state 'redux'}}
+let vendors = [
+    'react', 'react-router-dom', 'react-dom', 'prop-types', 'history', 'async-react-component',
+    'redux', 'react-redux', 'react-router-redux', 'redux-actions', 'redux-actions-promise'
+];
+{{/if_eq}}
+{{#if_eq state 'mobx'}}
+let vendors = [
+    'react', 'react-router-dom', 'react-dom', 'prop-types', 'history', 'async-react-component',
+    'mobx', 'mobx-react', 'mobx-react-router'
+];
+{{/if_eq}}
+
 module.exports = {
     entry: {
-        vendor: [
-            'react', 'react-router-dom', 'react-dom', 'prop-types', 'history', 'babel-polyfill'
-        ]
+        vendor: vendors
     },
     output: {
-        path: path.join(__dirname, './dist'),
+        path: path.join(__dirname, './{{path}}'),
         filename: '[name].dll.js',
         //定义输出：window.${output.library}
         library: '[name]_library'
     },
     plugins: [
         new webpack.DllPlugin({
-            path: path.join(__dirname, './dist', '[name]-manifest.json'),
+            path: path.join(__dirname, './{{path}}', '[name]-manifest.json'),
             // 和 output.library 一样即可
             name: '[name]_library'
         })

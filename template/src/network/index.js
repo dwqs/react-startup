a9faf0203e64b14f594bdb935340f49bc0bacfb9
dwqs <461147874@qq.com>
2018-08-15 13:11:34 +0800

    Update

--- template/src/network/index.js
+++ template/src/network/index.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
-console.log('api prefix', window.PREFIX);
+console.log('api prefix', window.PREFIX)
 
 const instance = axios.create({
   baseURL: window.PREFIX,
@@ -10,48 +10,38 @@ const instance = axios.create({
   },
   responseType: 'json',
   validateStatus: function (status) {
-    return status >= 200 && status < 500;
+    return status >= 200 && status < 500
   }
-});
+})
 
 export default {
   get (obj) {
     return Promise.resolve(
-        instance.get(obj.url,{
-            params: obj.data ? obj.data : {}
-        })
+      instance.get(obj.url, obj.data ? obj.data : {})
     )
   },
 
   post (obj) {
     return Promise.resolve(
-        instance.post(obj.url,{
-            data: obj.data ? obj.data : {}
-        })
+      instance.post(obj.url, obj.data ? obj.data : {})
     )
   },
 
   delete (obj) {
     return Promise.resolve(
-        instance.delete(obj.url,{
-            data: obj.data ? obj.data : {}
-        })
+      instance.delete(obj.url, obj.data ? obj.data : {})
     )
   },
 
   put (obj) {
     return Promise.resolve(
-        instance.put(obj.url,{
-            data: obj.data ? obj.data : {}
-        })
+      instance.put(obj.url, obj.data ? obj.data : {})
     )
   },
 
-  patch(obj){
+  patch (obj) {
     return Promise.resolve(
-        instance.patch(obj.url,{
-            data: obj.data ? obj.data : {}
-        })
+      instance.patch(obj.url, obj.data ? obj.data : {})
     )
   }
-};
+}
